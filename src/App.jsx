3cdf29845f4ff99ebce13b97d973f593eab87946
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-04-02 01:48:05 +0100

    personalização e funcao js

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,79 @@ import logo from "./assets/images/logo-ipb.svg";
 
 import "./assets/css/styles.css";
 
+import useLogin from "../src/assets/js/useLogin.js";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Spinner } from 'react-bootstrap';
+import {
+  faArrowRight,
+  faUserAlt,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
+
+
 export function App() {
+  
+  const { isLoading, onClickLogin } = useLogin();
+  
   return (
     <div className="container">
       <header className="header">
-        <img src={logo} alt="logo-ipb" />
+        <img src={logo} alt="logo-ipb" className="img-fluid" />
         <span>IPB.Orçamentos</span>
       </header>
 
-      <form>
-        <div className="inputContainer">
-          <input 
-          type="text" 
-          name="email" 
-          id="email" 
-          placeholder="Email" />
-        </div>
-
-        <div className="inputContainer">
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="Senha"
+      <Form>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1" className="icon-input">
+            <FontAwesomeIcon icon={faUserAlt}></FontAwesomeIcon>
+          </InputGroup.Text>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            aria-label="Email"
+            aria-describedby="basic-addon1"
+            className="custom-input rounded"
           />
-        </div>
+        </InputGroup>
 
-        <button className="button">Login</button>
-      </form>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon2" className="icon-input">
+            <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
+          </InputGroup.Text>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            aria-label="Password"
+            aria-describedby="basic-addon2"
+            className="custom-input rounded"
+          />
+        </InputGroup>
+
+        <Button
+          variant="primary"
+          type="submit"
+          className="button"
+          onClick={onClickLogin}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <Spinner animation="border" size="sm" />
+          ) : (
+            <>
+              Login <FontAwesomeIcon icon={faArrowRight} className="icon-btn" />
+            </>
+          )}
+        </Button>
+      </Form>
 
       <div>
-      <a href="">Esqueceste-te da tua conta?</a>
-      <a href="">Regista-te no IPB.Orçamentos</a>
+        <a href="">Esqueceste-te da tua conta?</a>
+        <a href="">Regista-te no IPB.Orçamentos</a>
       </div>
-      
     </div>
   );
 }
