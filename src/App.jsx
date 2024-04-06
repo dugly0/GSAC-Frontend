c71bc336fb206056bbcc91edde8f647dfe8ddce1
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-04-06 16:44:23 +0100

    criando pasta components e adicionando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,17 @@
-import logo from "./assets/images/logo-ipb.svg";
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/styles.css";
 
-import useLogin from "../src/assets/js/useLogin.js";
+import NavBar from "./components/navbar/Navbar";
 
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Spinner } from 'react-bootstrap';
-import {
-  faArrowRight,
-  faUserAlt,
-  faLock,
-} from "@fortawesome/free-solid-svg-icons";
+function App() {
+  const [count, setCount] = useState(0);
 
-
-export function App() {
-  
-  const { isLoading, onClickLogin } = useLogin();
-  
   return (
-    <div className="container">
-      <header className="header">
-        <img src={logo} alt="logo-ipb" className="img-fluid" />
-        <span>IPB.Orçamentos</span>
-      </header>
-
-      <Form>
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon1" className="icon-input">
-            <FontAwesomeIcon icon={faUserAlt}></FontAwesomeIcon>
-          </InputGroup.Text>
-          <Form.Control
-            type="email"
-            placeholder="Email"
-            aria-label="Email"
-            aria-describedby="basic-addon1"
-            className="custom-input rounded"
-          />
-        </InputGroup>
-
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon2" className="icon-input">
-            <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
-          </InputGroup.Text>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            aria-label="Password"
-            aria-describedby="basic-addon2"
-            className="custom-input rounded"
-          />
-        </InputGroup>
-
-        <Button
-          variant="primary"
-          type="submit"
-          className="button"
-          onClick={onClickLogin}
-          disabled={isLoading}
-        >
-          {isLoading ? (
-            <Spinner animation="border" size="sm" />
-          ) : (
-            <>
-              Login <FontAwesomeIcon icon={faArrowRight} className="icon-btn" />
-            </>
-          )}
-        </Button>
-      </Form>
-
-      <div>
-        <a href="">Esqueceste-te da tua conta?</a>
-        <a href="">Regista-te no IPB.Orçamentos</a>
-      </div>
+    <div className="App">
+      <NavBar />
     </div>
   );
 }
+
+export default App;
