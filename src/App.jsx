5a28cc82609988974be789556c154881a2bcc23a
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-04-06 16:51:24 +0100

    Import do header para App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/styles.css";
 
 import NavBar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <NavBar />
       <div className="container">
+        <Header />
         <Footer />
       </div>
     </div>
