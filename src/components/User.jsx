15af99c3e9ef7b36cf5fd323e3cad468f39c4e6f
a58071 <a58071@alunos.ipb.pt>
2024-05-28 16:33:14 +0100

    modal update faz get dos users

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,7 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-export default function User({ handleCollapseToggle }) {
+// User.js
+export default function User({ utilizador, username, handleCollapseToggle }) {
+  const displayName = utilizador?.nome || username;
   return (
     <div className="d-flex align-items-center ">
       <div className="flex-shrink-0">
@@ -10,7 +12,7 @@ export default function User({ handleCollapseToggle }) {
           <img className="img2" src="../../src/assets/imagem_perfil.png" />
         </a>
       </div>
-      <div className="flex-grow-1 ms-3">Fulana da Silva</div>
+      <div className="flex-grow-1 ms-3">{displayName}</div> 
       <div className="p-1" title="Editar Usuário">
         <FontAwesomeIcon
           cursor={"pointer"}
--- src/components/modalUpdate.jsx
+++ src/components/modalUpdate.jsx
@@ -1,12 +1,33 @@
-import { Collapse } from "react-bootstrap";
+import { Collapse, Alert } from "react-bootstrap"; // Added Alert for errors
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import User from "./User";
 import FormUpdate from "./formUpdate";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ModalUpdate({ isShow, handleClose }) {
   const [open, setOpen] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/user");
+        setUsers(response.data);
+        setHasError(false);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        setHasError(true);
+      }
+    };
+
+    if (isShow) {  // Fetch when modal is shown
+      fetchUsers();
+    }
+  }, [isShow]);
+
   const handleCollapseToggle = () => {
     setOpen(!open);
   };
@@ -19,12 +40,25 @@ export default function ModalUpdate({ isShow, handleClose }) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <User handleCollapseToggle={handleCollapseToggle} />
-        <Collapse in={open}>
-          <div id="example-collapse-text">
-            <FormUpdate />
-          </div>
-        </Collapse>
+        {hasError ? (
+          <Alert variant="danger">Error fetching users. Please try again.</Alert>
+        ) : (
+          <>
+            {users.map((user) => (
+              <div className="p-1 border"><User 
+                key={user.id}
+                handleCollapseToggle={handleCollapseToggle}
+                username={user.utilizador?.nome || user.username} // Use nome if available
+                {...user} // Pass all user data for future use
+              /></div>
+            ))}
+            <Collapse in={open}>
+              <div id="example-collapse-text">
+                <FormUpdate />
+              </div>
+            </Collapse>
+          </>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={handleClose}>Cancelar</Button>
