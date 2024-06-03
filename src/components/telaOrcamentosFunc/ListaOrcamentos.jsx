5e11f68df2ed62359827a957b5a965def902d0d7
Gabriel Rosario <a58917@alunos.ipb.pt>
2024-06-03 15:26:46 +0100

    separando-componente

--- src/components/telaOrcamentosFunc/ListaOrcamentos.jsx
+++ src/components/telaOrcamentosFunc/ListaOrcamentos.jsx
@@ -4,37 +4,8 @@ import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import MyVerticallyCenteredModal from './ModalStatus';
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-        <p>Item ID: {props.itemId}</p> {/* Exibe o item.id */}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 
 const endpoint = "http://localhost:8080/api/orcamento/orcamento-por-utilizador-id";
 const getToken = () => {
