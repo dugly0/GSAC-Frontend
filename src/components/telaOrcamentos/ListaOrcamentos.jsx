a65aa89ea767c4ff6373a1df04058f7b641e1e56
Robson Monteiro Serrao~ <a61196@alunos.ipb.pt>
2024-06-04 14:48:13 +0100

    inicializando get da tela cliente

--- src/components/telaOrcamentos/ListaOrcamentos.jsx
+++ src/components/telaOrcamentos/ListaOrcamentos.jsx
@@ -1,10 +1,66 @@
-import { Container } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container ,} from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
 import ModalButton from './ModalButton';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import MyVerticallyCenteredModal from './ModalStatus';
 
+const endpoint = "http://localhost:8080/api/orcamento/orcamento-por-utilizador-id";
+const getToken = () => {
+  const roleID = localStorage.getItem('role_id');
+  if(roleID != 2) {
+    alert('Você não tem permissão para acessar esta página.');  
+    return;
+  }
+  const token = localStorage.getItem('token');  
+  return token;
+}
+const getOrcamentos = async () => {
+  try {    
+    const token = getToken();
+    const result = await axios.get(endpoint, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    return result.data;
+  } catch (error) {
+    console.error("Failed to get token:", error);
+    throw error;
+  }
+};
 
 function BasicExample() {
+    const [orcamentos, setOrcamentos] = useState([]);
+    const [error, setError] = useState(null);
+    const [modalShow, setModalShow] = React.useState(false);
+    const [selectedItemId, setSelectedItemId] = React.useState(null);
+    const history = useNavigate();
+  
+    const handleShowModal = (itemId) => {
+      setSelectedItemId(itemId);
+      setModalShow(true);
+    };
+    useEffect(() => {   
+      const fetchData = async () => {      
+        try {
+          const data = await getOrcamentos();        
+          setOrcamentos(data);
+          setError(null);
+          
+        } catch (err) {
+          // setError('Failed to fetch orcamentos. Please check your credentials.');
+          // setOrcamentos([]);
+          history('/'); 
+          return;
+        }
+      };
+      fetchData();
+    }, [history]); // O array com history assegura que o efeito seja executado ao montar o componente e quando o objeto history mudar
+  
+
     
   return (
     <Container>
@@ -24,30 +80,21 @@ function BasicExample() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        </tr>
-                        <tr>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        </tr>
-                        <tr>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        </tr>
+                    {orcamentos.map((item, index) => (
+                  <tr key={index} >
+                    <td >{item.id}</td>
+                    <td>{item.data_entrada}</td>
+                    <td>{item.descricao}</td>
+                    <td>{item.preco}</td>
+                    <td>{item.data_entrega}</td>
+                    <td>{item.fatura}</td>
+                    <td>{item.utilizador_id}</td>
+                    <td>{item.laboratorio_id}</td>                    
+                    <td onClick={() => handleShowModal(item.id)}>Status</td>  
+                    <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false) }  itemId={selectedItemId}/>
+                                     
+                  </tr>
+                ))}                
                     </tbody>
             </Table>
         </Accordion.Body>
