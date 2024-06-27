487f1b65b0a3dc2ba1cc8f78b1aa0746544315f8
Gabriel Rosario <a58917@alunos.ipb.pt>
2024-06-27 18:29:58 +0100

    Atualizar estado

--- src/components/telaOrcamentosFunc/ModalStatus.jsx
+++ src/components/telaOrcamentosFunc/ModalStatus.jsx
@@ -7,59 +7,62 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from 'axios';
 import Form from "react-bootstrap/Form";
 
-
 function MyVerticallyCenteredModal(props) {
-  const { servicoOrcamentos, estadoOrcamentos  } = props.orcamento || {};
-  const [estados, setEstado] = useState([]);
-  const [estado_id, setEstadoId] = useState('');
+  const { servicoOrcamentos, estadoOrcamentos } = props.orcamento || {};
+  const [estados, setEstados] = useState([]);
+  const [estadoId, setEstadoId] = useState('');
   const [confirmDelete, setConfirmDelete] = useState(false); 
   const [show, setShow] = useState(false);
+
   const handleChangeEstado = (event) => {
-    setEstadoId(event.target.value); // Atualiza o estado_id com o valor selecionado
+    setEstadoId(event.target.value); // Atualiza o estadoId com o valor selecionado
   };
+
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    setShow(true) 
-    getEstados()
+    setShow(true);
+    getEstados();
   };
+
   const getToken = () => {
-    
     const token = localStorage.getItem('token');  
     return token;
-  }
+  };
+
   const deleteServico = async (id) => {
-    try {    
-        const token = getToken();
-        const result = await axios.delete('http://localhost:8080/api/orcamento/delete-servico-orcamento', {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-            data: {
-                id: id
-            }
-        });
-        console.log("Serviço excluído:", result.data);
-        return result.data;
+    try {
+      const token = getToken();
+      const result = await axios.delete('http://localhost:8080/api/orcamento/delete-servico-orcamento', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        data: { id }
+      });
+      console.log("Serviço excluído:", result.data);
+      return result.data;
     } catch (error) {
-        console.error("Failed to delete service:", error);
-        throw error;
+      console.error("Failed to delete service:", error);
+      throw error;
     }
-};
+  };
+
   const handleDeleteClick = () => {
     setConfirmDelete(true); // Mostra o botão de confirmação de exclusão
-  }; 
-  const handleConfirmDelete = (servico) => {
-    deleteServico(servico.id)
+  };
+
+  const handleConfirmDelete = async (servico) => {
+    await deleteServico(servico.id);
     setConfirmDelete(false); // Esconde o botão de confirmação após a exclusão
     props.onHide();
     window.location.reload(); // Recarrega a página
   };
+
   const handleCancelDelete = () => {
     setConfirmDelete(false); // Cancela a exclusão e esconde o botão de confirmação
   };
-  const renderServicos = () => ( 
-    
+
+  const renderServicos = () => (
     <Table>
       <thead>
         <tr>
@@ -72,70 +75,69 @@ function MyVerticallyCenteredModal(props) {
         </tr>
       </thead>
       <tbody>
-        {servicoOrcamentos.map((servico)=> (
+        {servicoOrcamentos.map((servico) => (
           <tr key={servico.id}>
             <td>{servico.nome}</td>
             <td>{servico.descricao}</td>
             <td>{servico.preco_unitario_custo}</td>
             <td>{servico.preco_unitario_venda}</td>
-            <td>{servico.quantidade}</td>            
+            <td>{servico.quantidade}</td>
             <td>
-                {!confirmDelete ? (
+              {!confirmDelete ? (
                 <a href="#deleteConfirm" onClick={handleDeleteClick}>
                   <FontAwesomeIcon icon={faTimes} />
                 </a>
-                ) : (
-                  <div>
-                    <span>Confirmar exclusão?</span>
-                    <button className="btn btn-sm btn-danger ms-2" 
-                    onClick={()=>handleConfirmDelete(servico)}>Confirmar</button>
-                    <button className="btn btn-sm btn-secondary ms-2" onClick={handleCancelDelete}>Cancelar</button>
-                  </div>
-                )}
+              ) : (
+                <div>
+                  <span>Confirmar exclusão?</span>
+                  <button className="btn btn-sm btn-danger ms-2" onClick={() => handleConfirmDelete(servico)}>Confirmar</button>
+                  <button className="btn btn-sm btn-secondary ms-2" onClick={handleCancelDelete}>Cancelar</button>
+                </div>
+              )}
             </td>
           </tr>
         ))}
-        </tbody>       
-  </Table>
+      </tbody>
+    </Table>
   );
-  const token = getToken();
-  const getEstados = async (key) => {
-    try {    
-      // const token = getToken();
-      const result = await axios.get("http://localhost:8080/api/estado", {
-          headers: {
-              'Authorization': `Bearer ${token}`
-          }
-      });
-      setEstado(result.data);
-    } catch (error) {
-      console.error("Failed to get token:", error);
-      throw error;
-    }
-  };
-  const handleSubmit = async () => {
-    try {    
+
+  const getEstados = async () => {
+    try {
       const token = getToken();
-      const result = await axios.put(`http://localhost:8080/api/orcamento/update-estado` , {
+      const result = await axios.get("http://localhost:8080/api/estado", {
         headers: {
           'Authorization': `Bearer ${token}`
-      } ,
-          orcamentoId: props.orcamento.id,         
-          estadoOrcamentos: estado_id
-          
+        }
       });
-      console.log(token , estado_id)
-      setShow(false); //
-      return result.data;
-     
+      setEstados(result.data);
     } catch (error) {
-      console.error("Failed", error);
-      console.log(token , estado_id)
+      console.error("Failed to get estados:", error);
       throw error;
     }
-    setShow(false); // Feche o modal após salvar
   };
- 
+
+  const handleSubmit = async () => {
+    try {
+      const token = getToken();
+      const result = await axios.put("http://localhost:8080/api/orcamento/update-estado", {
+        orcamentoId: props.orcamento.id,
+        estadoOrcamentos: estadoId
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log("Token:", token, "Estado ID:", estadoId);
+      setShow(false); // Feche o modal após salvar
+      return result.data;
+    } catch (error) {
+      console.error("Failed to update estado:", error);
+      console.log("Token:", token, "Estado ID:", estadoId);
+      throw error;
+    }
+  };
+
   const renderEstados = () => (
     <Table>
       <thead>
@@ -153,16 +155,11 @@ function MyVerticallyCenteredModal(props) {
         ))}
       </tbody>
     </Table>
-  );  
-  
+  );
+
   return (
     <>
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
+      <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
             Serviços e Estados
@@ -182,40 +179,39 @@ function MyVerticallyCenteredModal(props) {
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Fechar</Button>
-          <Button variant="primary" onClick={handleShow} >
+          <Button variant="primary" onClick={handleShow}>
             Alterar Estado
           </Button>
-          </Modal.Footer>
+        </Modal.Footer>
       </Modal>
-
 
       <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Alterar Estado</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form.Group controlId="Estado">
-          <Form.Label>Estado</Form.Label>
-          <Form.Select  onChange={handleChangeEstado} value={estado_id}>
-            <option value="">Selecione um estado</option>
-            {estados.map((estado) => (
-              <option key={estado.id} value={estado.id}>
-                {estado.estado}
-              </option>
-            ))}
-          </Form.Select>
-        </Form.Group> 
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleSubmit}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+        <Modal.Header closeButton>
+          <Modal.Title>Alterar Estado</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group controlId="Estado">
+            <Form.Label>Estado</Form.Label>
+            <Form.Select onChange={handleChangeEstado} value={estadoId}>
+              <option value="">Selecione um estado</option>
+              {estados.map((estado) => (
+                <option key={estado.id} value={estado.id}>
+                  {estado.estado}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
       </Modal>
-  </>
+    </>
   );
 }
 
